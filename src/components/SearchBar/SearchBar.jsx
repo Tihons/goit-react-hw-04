9b8a976b0f5cb38012a9f90d1221795b34a102d7
Tihons <tihonshramenko@gmail.com>
2024-04-08 21:00:10 +0300

    save

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,25 +2,26 @@ import css from "./SearchBar.module.css";
 import toast from "react-hot-toast";
 
 export const SearchForm = ({ onSearch }) => {
-  const hendelSubmit = (e) => {
+  const handleScroll = (e) => {
+    console.dir(e);
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    onSearch(e.target.elements.query.value);
 
     if (e.target.elements.query.value.trim() === "") {
       toast.error("EMPTY STRING");
       return;
     }
-    e.target.reset();
-  };
 
-  const handleScrol = (e) => {
-    console.dir(e);
+    onSearch(e.target.elements.query.value);
+    e.target.reset();
   };
 
   return (
     <>
-      <header onScrollCapture={handleScrol} className={css.headerBox}>
-        <form className={css.headerForm} onSubmit={hendelSubmit}>
+      <header onScrollCapture={handleScroll} className={css.headerBox}>
+        <form className={css.headerForm} onSubmit={handleSubmit}>
           <input
             type="text"
             autoComplete="off"
